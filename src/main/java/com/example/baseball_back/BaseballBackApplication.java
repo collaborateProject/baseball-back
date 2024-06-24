e41738e8f72fc3d379bca580607fc82d84e6c246
chohuison <117262031+chohuison@users.noreply.github.com>
2024-06-25 03:07:26 +0900

    .

---
+++ src/main/java/com/example/baseball_back/BaseballBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.baseball_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseballBackApplication.class, args);
+	}
+
+}
