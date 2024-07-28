6f4b89db55724b85a559dfdd7a903196dae8bef5
chohuison <117262031+chohuison@users.noreply.github.com>
2024-07-28 13:29:38 +0900

    redispassword

--- src/main/java/com/example/baseball_back/config/RedisConfig.java
+++ src/main/java/com/example/baseball_back/config/RedisConfig.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -17,10 +19,14 @@ public class RedisConfig {
 
     @Value("${spring.redis.port}")
     private int port;
+    @Value("${spring.redis.password}")
+    public String redisPassword;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, port);
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(redisPassword));
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
